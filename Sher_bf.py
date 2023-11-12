bcd6c2da235c282e603cb8b2cc83abdac4044a25
Kovertechcod3 <144227649+Kovertechcod3@users.noreply.github.com>
2023-11-12 07:38:05 -0800

    Update Sher_bf.py

--- Sher_bf.py
+++ Sher_bf.py
@@ -1,81 +1,90 @@
-import time
 import itertools
-import logging
-from multiprocessing import Pool
-from tqdm import tqdm
 import bcrypt
 
-# Set the character sets to use for generating passwords
+# Configuration
 uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
 numbers = "0123456789"
 special_characters = "!@#$%^&*()_+-=[]{}|;':,.<>?/`~"
 
-# Set the password criteria
 MIN_PASSWORD_LENGTH = 8
 MIN_UPPERCASE_LETTERS = 1
 MIN_LOWERCASE_LETTERS = 1
 MIN_NUMBERS = 1
 MIN_SPECIAL_CHARACTERS = 1
 
+DEFAULT_PASSWORDS = [
+    # Telus default passwords
+    "telus",
+    "telus123",
+    "telusadmin",
+    "telusadmin123",
+    "teluswifi",
+    "teluswifi123",
+    "teluswireless",
+    "teluswireless123",
+    # Shaw default passwords
+    "shaw",
+    "shaw123",
+    "shawadmin",
+    "shawadmin123",
+    "shawwifi",
+    "shawwifi123",
+    "shawwireless",
+    "shawwireless123"
+]
+
 def check_password_strength(password):
-    # Check if the password meets the criteria
-    if (
+    return (
         len(password) >= MIN_PASSWORD_LENGTH
         and sum(1 for c in password if c.isupper()) >= MIN_UPPERCASE_LETTERS
         and sum(1 for c in password if c.islower()) >= MIN_LOWERCASE_LETTERS
         and sum(1 for c in password if c.isdigit()) >= MIN_NUMBERS
         and sum(1 for c in password if c in special_characters) >= MIN_SPECIAL_CHARACTERS
-    ):
-        return True
-    return False
+    )
 
-def brute_force_telus_shaw_wifi():
-    password = ""
-    target_password = b"SuperSecretPassword"  # Hashed target password
+def brute_force_telus_shaw_wifi(target_password):
+    # Check the default passwords first
+    for default_password in DEFAULT_PASSWORDS:
+        if bcrypt.checkpw(default_password.encode(), target_password):
+            print(f"Success! Password found: {default_password}")
+            return
 
-    # Define the character sets to use for generating passwords
-    character_sets = [uppercase_letters, lowercase_letters, numbers, special_characters]
+    char_sets = [
+        uppercase_letters,
+        lowercase_letters,
+        numbers,
+        special_characters
+    ]
 
-    # Set up logging
-    logging.basicConfig(filename='brute_force.log', level=logging.INFO)
+    max_len = len(target_password)
+    for password_length in range(MIN_PASSWORD_LENGTH, max_len + 1):
+        combinations = itertools.product(*char_sets, repeat=password_length)
+        combinations = itertools.islice(combinations, len(char_sets)**password_length)
 
-    # Set the maximum number of processes to use in parallel processing
-    num_processes = 4
+        for combination in combinations:
+            password = "".join(combination)
+            if check_password_strength(password) and bcrypt.checkpw(password.encode(), target_password):
+                print(f"Success! Password found: {password}")
+                return
 
-    # Generate passwords of varying lengths
-    with Pool(processes=num_processes) as pool:
-        for password_length in range(MIN_PASSWORD_LENGTH, len(target_password) + 1):
-            # Generate all possible combinations of characters
-            combinations = itertools.product(*character_sets, repeat=password_length)
-            pbar = tqdm(total=len(character_sets)**password_length, unit=' Combination(s)', desc=f"Length: {password_length}")
+def generate_passwords(length):
+    char_sets = [
+        uppercase_letters,
+        lowercase_letters,
+        numbers,
+        special_characters
+    ]
 
-            # Use multiprocessing to speed up the brute force process
-            results = pool.imap(try_password, combinations)
-
-            for result in results:
-                if result is not None:
-                    password = "".join(result)
-                    pbar.close()
-                    logging.info(f"Success! Password found: {password}")
-                    print(f"Success! Password found: {password}")
-                    return
-
-                pbar.update()
-
-            pbar.close()
-
-    logging.info("Password not found.")
-    print("Password not found.")
-
-def try_password(combination):
-    password = "".join(combination)
-    if check_password_strength(password):
-        # Add delay between each attempted password
-        time.sleep(0.1)
-        # Hash the attempted password and compare with the target password hash
-        if bcrypt.checkpw(password.encode(), target_password):
-            return combination
+    combinations = itertools.product(*char_sets, repeat=length)
+    passwords = ("".join(combination) for combination in combinations if check_password_strength("".join(combination)))
+    return passwords
 
 if __name__ == "__main__":
-    brute_force_telus_shaw_wifi()
+    target_password = b"SuperSecretPassword"
+    brute_force_telus_shaw_wifi(target_password)
+
+    # Generate passwords of length 10
+    passwords = generate_passwords(10)
+    for password in passwords:
+        print(password)
