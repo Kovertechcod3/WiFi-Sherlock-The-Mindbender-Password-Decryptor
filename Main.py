f788afb61165f0a9655b471338658075b2d7f0d0
Kovertechcod3 <144227649+Kovertechcod3@users.noreply.github.com>
2023-12-02 08:42:18 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,11 @@
 import itertools
 import bcrypt
-import argparse
 
 # Configuration
 uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
 numbers = "0123456789"
 special_characters = "!@#$%^&*()_+-=[]{}|;':,.<>?/`~"
-
-def decrypt_password(password_hash):
-    # Implement the password decryption algorithm here
-    # Use the password_hash variable to crack the password
-
-    # Your code here...
-    pass
 
 MIN_PASSWORD_LENGTH = 8
 MIN_UPPERCASE_LETTERS = 1
@@ -22,7 +14,6 @@ MIN_NUMBERS = 1
 MIN_SPECIAL_CHARACTERS = 1
 
 DEFAULT_PASSWORDS = [
-    # Telus default passwords
     "telus",
     "telus123",
     "telusadmin",
@@ -31,15 +22,6 @@ DEFAULT_PASSWORDS = [
     "teluswifi123",
     "teluswireless",
     "teluswireless123",
-    # Shaw default passwords
-    "shaw",
-    "shaw123",
-    "shawadmin",
-    "shawadmin123",
-    "shawwifi",
-    "shawwifi123",
-    "shawwireless",
-    "shawwireless123"
 ]
 
 def check_password_strength(password):
@@ -51,7 +33,11 @@ def check_password_strength(password):
         and sum(1 for c in password if c in special_characters) >= MIN_SPECIAL_CHARACTERS
     )
 
-def brute_force_telus_shaw_wifi(target_password):
+def generate_password_combinations(char_sets, password_length):
+    combinations = itertools.product(*char_sets, repeat=password_length)
+    return itertools.islice(combinations, len(char_sets)**password_length)
+
+def brute_force_telus_password(target_password):
     # Check the default passwords first
     for default_password in DEFAULT_PASSWORDS:
         if bcrypt.checkpw(default_password.encode(), target_password):
@@ -67,8 +53,7 @@ def brute_force_telus_shaw_wifi(target_password):
 
     max_len = len(target_password)
     for password_length in range(MIN_PASSWORD_LENGTH, max_len + 1):
-        combinations = itertools.product(*char_sets, repeat=password_length)
-        combinations = itertools.islice(combinations, len(char_sets)**password_length)
+        combinations = generate_password_combinations(char_sets, password_length)
 
         for combination in combinations:
             password = "".join(combination)
@@ -76,47 +61,5 @@ def brute_force_telus_shaw_wifi(target_password):
                 print(f"Success! Password found: {password}")
                 return
 
-def generate_passwords(length):
-    char_sets = [
-        uppercase_letters,
-        lowercase_letters,
-        numbers,
-        special_characters
-    ]
-
-    combinations = itertools.product(*char_sets, repeat=length)
-    passwords = ("".join(combination) for combination in combinations if check_password_strength("".join(combination)))
-    return passwords
-
-def main():
-    parser = argparse.ArgumentParser(description='Password Cracking Tool for Telus and Shaw WiFi Networks')
-    parser.add_argument('target_password', type=str, help='Target password to crack')
-    parser.add_argument('--length', type=int, default=10, help='Length of generated passwords')
-
-    args = parser.parse_args()
-
-    target_password = args.target_password.encode()
-
-    brute_force_telus_shaw_wifi(target_password)
-
-    if args.length:
-        passwords = generate_passwords(args.length)
-        for password in passwords:
-            print(password)
-    else:
-        parser.print_help()
-
-if __name__ == "__main__":
-    main()
-```
-
-In this updated code, I made the following changes:
-- Removed the `hashlib` import since it wasn't used in the updated code.
-- Removed the `decrypt_password` function since it wasn't implemented and not used in the code.
-- Updated the code to include the password cracking logic for Telus and Shaw WiFi networks using the `brute_force_telus_shaw_wifi` function.
-- Updated the `main` function to use the `argparse` module and parse the command-line arguments.
-- Removed the `password_hash` variable since it wasn't used in the updated code.
-- Removed the `decrypted_password` variable and related code since it wasn't used in the updated code.
-- Removed the `else` block in the `main` function that printed the help message since it's not necessary.
-
-Please note that the password cracking logic in the `brute_force_telus_shaw_wifi` function assumes the use of the `bcrypt` library for password hashing. Make sure you have the `bcrypt` library installed before running the code.
+target_password = input("Enter the target password: ").encode()
+brute_force_telus_password(target_password)
